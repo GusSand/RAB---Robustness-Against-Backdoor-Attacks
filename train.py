0ba09bdfa19b7f72f98e209872d94226dc09253f
Prof. G. Sandoval <gussand@gmail.com>
2022-04-25 14:26:34 -0400

    add logs

--- train.py
+++ train.py
@@ -20,7 +20,8 @@ parser.add_argument('--poison_r', type=float, default=0.0)
 parser.add_argument('--delta', type=float, default=1.0)
 
 # Smoothing Setting
-parser.add_argument('--N_m', type=int, default=1000)
+#parser.add_argument('--N_m', type=int, default=1000)
+parser.add_argument('--N_m', type=int, default=10)
 parser.add_argument('--sigma', type=float, default=0.0)
 parser.add_argument('--dldp_sigma', type=float, default=0.0)
 parser.add_argument('--dldp_gnorm', type=float, default=5.0)
@@ -44,13 +45,19 @@ if __name__ == '__main__':
     if not os.path.isdir(PREFIX):
         os.makedirs(PREFIX)
 
-    for _ in range(args['N_m']):
-        model = Model(gpu=use_gpu)
-        trainset = SmoothedDataset(poisoned_train, args['sigma'])
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
-        train_model(model, trainloader, lr=LR, epoch_num=N_EPOCH, dldp_setting=(args['dldp_sigma'],args['dldp_gnorm']), verbose=False)
-        save_path = PREFIX+'/smoothed_%d.model'%_
-        torch.save(model.state_dict(), save_path)
-        acc_benign = eval_model(model, testloader_benign)
-        acc_poison = eval_model(model, testloader_poison)
-        print ("Benign/Poison ACC %.4f/%.4f, saved to %s @ %s"%(acc_benign, acc_poison, save_path, datetime.now()))
+    filename = './logs/%s-%s-delta%.4f-poison_r%.4f.csv'%(args['dataset'], args['atk_method'], args['delta'], args['poison_r'])
+    if not os.path.isdir('./logs'):
+        os.makedirs('./logs')
+    with open(filename, 'w') as f: 
+        f.write("Benign, Poison\n")
+        for _ in range(args['N_m']):
+            model = Model(gpu=use_gpu)
+            trainset = SmoothedDataset(poisoned_train, args['sigma'])
+            trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+            train_model(model, trainloader, lr=LR, epoch_num=N_EPOCH, dldp_setting=(args['dldp_sigma'],args['dldp_gnorm']), verbose=False)
+            save_path = PREFIX+'/smoothed_%d.model'%_
+            torch.save(model.state_dict(), save_path)
+            acc_benign = eval_model(model, testloader_benign)
+            acc_poison = eval_model(model, testloader_poison)
+            print ("Benign/Poison ACC %.4f/%.4f, saved to %s @ %s"%(acc_benign, acc_poison, save_path, datetime.now()))
+            f.write(f"{acc_benign}, {acc_poison}\n")
